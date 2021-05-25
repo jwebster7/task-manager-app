36e91331c2f6ac82bdfbc4679b7028d4aaeeabcc
jwebster7 <>
2021-05-24 22:05:32 -0500

    refactored auth middleware and some routes to use cookies instead of sending a token. added an expiration to each generated token. installed cookie-parser and cors.

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,8 @@
-const express = require("express");
 require("./db/mongoose");
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const taskRouter = require("./routers/task");
 const userRouter = require("./routers/user");
@@ -7,6 +10,8 @@ const userRouter = require("./routers/user");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 app.use(taskRouter);
 app.use(userRouter);
 
